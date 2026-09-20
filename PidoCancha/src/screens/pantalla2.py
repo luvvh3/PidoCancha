8c6cd7748c46eccb9386fae64a9df1d8151ffc86
luisfvh3 <luisfvh3@gmail.com>
2026-09-20 10:57:50 -0400

    Versión inicial

---
+++ PidoCancha/src/screens/pantalla2.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+def pantalla2Mostrar():
+    return ft.Column(
+        controls=[
+            ft.Text(
+                
+            )
+        ]
+    )
---
+++ PidoCancha/src/screens/pantalla3.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+def pantalla3Mostrar():
+    return ft.Column(
+        controls=[
+            ft.Text(
+                
+            )
+        ]
+    )
